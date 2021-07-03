30ec0ccac55f27c3c939b5c676d4e8f81aa0f08d
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2021-07-03 11:44:44 +0200

    working minimal example

--- coroutine.hpp
+++ coroutine.hpp
@@ -1,69 +1,59 @@
 #include <coroutine>
 #include <iostream>
 
-using namespace std::experimental::coroutines_v1;
-
-template<typename T>
+template <typename T>
 struct Promise;
-template<typename T>
+template <typename T>
 class Generator;
 
-template<typename T>
-using Handle = std::experimental::coroutines_v1::coroutine_handle<Promise<T>>;
+template <typename T>
+using Handle = std::coroutine_handle<Promise<T>>;
 
-template<typename T>
-struct Promise
-{
-    Promise() : val { T() } {}
-    std::experimental::coroutines_v1::suspend_always initial_suspend() { return {}; }
-    std::experimental::coroutines_v1::suspend_always final_suspend() { return {}; }
-    Generator<T> get_return_object();
-    void unhandled_exception() { abort(); }
-    void return_value(T val) {
-        this->val = val;
-    }
-    std::experimental::coroutines_v1::suspend_always yield_value(T val) {
-        this->val = val;
-        return {};
-    }
+template <typename T>
+struct Promise {
+  Promise() : val{T()} {}
+  std::suspend_always initial_suspend() noexcept { return {}; }
+  std::suspend_always final_suspend() noexcept { return {}; }
+  Generator<T> get_return_object();
+  void unhandled_exception() { abort(); }
+  void return_value(T val) { this->val = val; }
+  std::suspend_always yield_value(T val) {
+    this->val = val;
+    return {};
+  }
 
-    T val;
+  T val;
 };
 
-template<typename T>
-class Generator
-{
-public:
-        explicit Generator(Handle<T> handle)
-        : m_handle (handle)
-    {}
-    ~Generator() {
-        if (m_handle) {
-            m_handle.destroy();
-        }
-    }
-    T next();
-    using promise_type = Promise<T>;
-private:
-    Handle<T> m_handle;
-};
-
-template<typename T>
-Generator<T> Promise<T>::get_return_object()
-{
-    return Generator<T> { Handle<T>::from_promise(*this) };
-}
-
-
-template<typename T>
-T Generator<T>::next()
-{
+template <typename T>
+class Generator {
+ public:
+  explicit Generator(Handle<T> handle) : m_handle(handle) {}
+  ~Generator() {
     if (m_handle) {
-        m_handle.resume();
-        return m_handle.promise().val;
-    } else {
-        return T();
+      m_handle.destroy();
     }
+  }
+  T next();
+  using promise_type = Promise<T>;
+
+ private:
+  Handle<T> m_handle;
+};
+
+template <typename T>
+Generator<T> Promise<T>::get_return_object() {
+  return Generator<T>{Handle<T>::from_promise(*this)};
 }
 
-//End general purpose library
+template <typename T>
+T Generator<T>::next() {
+  if (m_handle) {
+    m_handle.resume();
+    return m_handle.promise().val;
+  } else {
+    return T();
+  }
+}
+
+// End general purpose library
--- main_hello.cpp
+++ main_hello.cpp
@@ -1,24 +1,35 @@
 #include "coroutine.hpp"
 
-Generator<std::int64_t> getPrimes()
-{
-    std::int64_t num = 0;
-    while (true) {
-        while (!isPrime(num)) {
-            ++num;
-        }
-        co_yield num;
-        ++num;
-    }
-    co_return 0;
+bool isPrime(std::int64_t n) {
+  if (n == 2 || n == 3) return true;
+
+  if (n <= 1 || n % 2 == 0 || n % 3 == 0) return false;
+
+  for (int i = 5; i * i <= n; i += 6) {
+    if (n % i == 0 || n % (i + 2) == 0) return false;
+  }
+
+  return true;
 }
 
-
-int main()
-{
-    Generator<int64_t> primes = getPrimes();
-    for (int i = 0; i < 100; ++i) {
-        std::cout << primes.next() << " is prime\n";
+Generator<std::int64_t> getPrimes() {
+  std::int64_t num = 0;
+  while (true) {
+    while (!isPrime(num)) {
+      ++num;
     }
-    return 0;
+    co_yield num;
+    ++num;
+  }
+  co_return 0;
+}
+
+int main() {
+  Generator<int64_t> primes = getPrimes();
+  std::cout << primes.next();
+  for (int i = 0; i < 1000000; ++i) {
+    std::cout << ", " << primes.next();
+  }
+  std::cout << '\n';
+  return 0;
 }
